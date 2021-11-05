c2c595764faa3ff34fcd3fbd9c6456836cbf0bf1
DavidLivingstoneHini <kwamelivingstone77@gmail.com>
2021-11-05 00:12:21 +0000

    React Native Sneaker Shop IA

--- src/screens/Detail.js
+++ src/screens/Detail.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import {View,Text,Image,TouchableOpacity,StyleSheet, ImageBackground} from 'react-native'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-import SwiperComponent from '../common/SwiperComponent'
+import React from 'react';
+import {View,Text,Image,TouchableOpacity,StyleSheet, ImageBackground} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import SwiperComponent from '../common/SwiperComponent';
 import Stars from 'react-native-stars';
 import { ScrollView } from 'react-native-gesture-handler';
 
